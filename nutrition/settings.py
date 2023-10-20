6e2f304e4d78b3dc7914b5e40067e7b3f8b6f393
hughes84 <134289376+hughes84@users.noreply.github.com>
2023-10-20 15:44:49 +0100

    Update allowed hosts

--- nutrition/settings.py
+++ nutrition/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://nutrition2023-ea03d13919e5.herokuapp.com/"]
 
 
 # Application definition
