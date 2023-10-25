12067dfb17a491f98fd6f43654e9f6f05813ca39
hughes84 <134289376+hughes84@users.noreply.github.com>
2023-10-25 17:22:16 +0100

    Chore: Add cloudinary and summernote to install apps and add static and media url path

--- nutrition/settings.py
+++ nutrition/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://nutrition2023-ea03d13919e5.herokuapp.com/"]
+ALLOWED_HOSTS = ["https://nutrition2023-ea03d13919e5.herokuapp.com/" , "127.0.0.1"]
 
 
 # Application definition
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary',
+    'django_summernote',
     'blog',
 ]
 
@@ -60,7 +62,7 @@ ROOT_URLCONF = 'nutrition.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -128,6 +130,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
