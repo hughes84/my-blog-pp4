8662fbc404618499af5280c05cf0ddf5e57a7075
hughes84 <134289376+hughes84@users.noreply.github.com>
2023-11-10 17:00:05 +0000

    Add urls for about us, homeview, contact, recipeview and recipedetail

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,15 @@
 from . import views
 from django.urls import path
 
+
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home'),
+    path('', views.HomeView.as_view(), name='home'),
+    path('posts/', views.PostList.as_view(), name='posts'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
-]
+    path('aboutus', views.AboutUs.as_view(), name='aboutus'),
+    path('contact', views.Contact.as_view(), name='contact'),
+    path('recipes', views.RecipesView.as_view(), name='recipes'),
+    path('recipedetail/<int:recipe_id>/', views.RecipeDetailView.as_view(), name='recipedetail'),
+
+] 
