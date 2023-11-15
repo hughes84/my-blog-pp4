52ab1926b25df2c02eff8730978d870fae11f738
hughes84 <134289376+hughes84@users.noreply.github.com>
2023-11-15 11:47:50 +0000

    Add contact form for contact page

--- blog/forms.py
+++ blog/forms.py
@@ -5,4 +5,8 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('body',)
-        
+
+class ContactForm(forms.Form):
+    name = forms.CharField(max_length=100)
+    email = forms.EmailField()
+    message = forms.CharField(widget=forms.Textarea)
